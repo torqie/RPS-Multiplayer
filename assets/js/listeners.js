3e09107755899ea86f0612efbc0559c30445d45b
Terik Hone <terikhone@gmail.com>
2020-04-06 02:08:45 -0600

    fixed error when name was deleted

--- assets/js/listeners.js
+++ assets/js/listeners.js
@@ -6,7 +6,7 @@ fb.database.ref("players").on("child_added", function(data) {
   console.log("Key: ", key);
   console.log("Data: ", player);
 
-  $("#" + key + " h4.name").text(player.name.length > 0 ? player.name : "");
+  $("#" + key + " h4.name").text(typeof player.name != "undefined" ? player.name : "");
   $("#" + key + "-wins span").text(player.wins);
   $("#" + key + "-ties span").text(player.ties);
   $("#" + key + "-losses span").text(player.losses);
@@ -16,7 +16,7 @@ fb.database.ref("players").on("child_changed", function(data) {
   const key = data.key;
   const player = data.val();
 
-  $("#" + key + " h4.name").text(player.name.length > 0 ? player.name : "");
+  $("#" + key + " h4.name").text(typeof player.name != "undefined" ? player.name : "");
   $("#" + key + "-wins span").text(player.wins);
   $("#" + key + "-ties span").text(player.ties);
   $("#" + key + "-losses span").text(player.losses);
--- assets/js/player.js
+++ assets/js/player.js
@@ -6,16 +6,20 @@ const player = {
 
   setPlayer(name) {
     // Check if player1 Exists.. If so create player 2
-    this.database.ref(this.playerOneRef + "/name").once("value").then(function (snapshot) {
+    this.database.ref("players").once("value").then(function (snapshot) {
+
+      console.log(snapshot.val()["player-one"].name);
+      const playerOneName = snapshot.val()["player-one"].name;
+      const playerTwoName = snapshot.val()["player-two"].name;
 
       const newPlayer = {
-        player: null,
+        player: "Guest",
         name: name,
       };
       // Check if player1 exists.
-      if(snapshot.val() == null) {
+      if(typeof playerOneName == "undefined") {
         newPlayer.player = "player-one";
-      } else {
+      } else if(typeof playerTwoName == "undefined") {
         // Does exist, create player2
         newPlayer.player = "player-two";
       }
