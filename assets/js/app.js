a91b6602df69a6072ff094461fe00dbfd6e70401
Terik Hone <terikhone@gmail.com>
2020-04-10 22:37:25 -0600

    added images for Rock, Paper, Scissor

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,7 +18,7 @@ $(function() {
     const choice = $(this).attr("data-type"); //rock, paper, scissors
 
     // Disable choice buttons
-    $(this).parent().children().prop("disabled", true);
+    $(".choice").prop("disabled", true);
 
     // Update firebase with players choice
     player.setChoice(choice);
